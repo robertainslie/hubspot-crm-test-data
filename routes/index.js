40f4fc69ab1b3f8335519ac0f4602780bb4699f5
robertainslie <robert.p.ainslie@gmail.com>
2018-08-22 20:49:39 -0400

    added error m to template

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Install Test Data Into HubSpot CRM' });
 });
 
-router.get('/success', function(req, res, next) {
-  res.render('success', { title: 'Success!' });
+router.get('/auth', function(req, res, next) {
+    if (req.params.code.length > 0) {
+    	res.render('success', { title: 'Success!', auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.', code:req.params.code});
+    }
+    else{
+    	res.render('success', { title: 'There was an issue.',auth_message:'Something went wrong.' });
+    }
 });
 
 module.exports = router;
