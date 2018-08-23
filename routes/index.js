138d72189ea402f72b0b379259543b28bdeec139
robertainslie <robert.p.ainslie@gmail.com>
2018-08-22 22:02:13 -0400

    qs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var qs = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,17 +12,17 @@ router.get('/auth', function(req, res, next) {
 	console.log(req.query.code);
 	console.log(req.query.code.length);
     if (req.query.code.length > 0) {
-    	axios.post('https://api.hubapi.com/oauth/v1/token', {
+    	axios.post('https://api.hubapi.com/oauth/v1/token', qs.stringify({
 		    grant_type:'authorization_code',
 		    client_id:process.env.HS_CLIENT_ID,
 		    client_secret:process.env.HS_CLIENT_SECRET,
 		    redirect_uri:'https://hs-test-data.herokuapp.com/',
 		    code:req.query.code
-		  })
+		  }))
 		  .then(function (response) {
 		  	var access_token = response.access_token
 		    console.log(response);
-		    res.render('success', { title: 'Success!', access_token:response.access_token,auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.', code:req.query.code});
+		    res.render('success', { title: 'Success!', access_token:response.access_token, auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.'});
 		  })
 		  .catch(function (error) {
 		    console.log(error);
