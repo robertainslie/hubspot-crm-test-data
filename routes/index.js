65e475b9adeb3dae8cf528322cad8853bbb25026
robertainslie <robert.p.ainslie@gmail.com>
2018-08-22 21:11:58 -0400

    fixing query

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/auth', function(req, res, next) {
-	console.log(req.params.code)
-	console.log(req.params.code.length)
-    if (req.params.code.length > 0) {
-    	res.render('success', { title: 'Success!', auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.', code:req.params.code});
+	console.log(req.query.code);
+	console.log(req.query.code.length);
+    if (req.query.code.length > 0) {
+    	res.render('success', { title: 'Success!', auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.', code:req.query.code});
     }
     else{
     	res.render('success', { title: 'There was an issue.',auth_message:'Something went wrong.' });
