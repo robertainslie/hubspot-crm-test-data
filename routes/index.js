82121c9005dfb593bd856f1781072d96116e15d7
robertainslie <robert.p.ainslie@gmail.com>
2018-08-22 20:58:24 -0400

    code param

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/auth', function(req, res, next) {
+	console.log(req.params.code)
+	console.log(req.params.code.length)
     if (req.params.code.length > 0) {
     	res.render('success', { title: 'Success!', auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.', code:req.params.code});
     }
