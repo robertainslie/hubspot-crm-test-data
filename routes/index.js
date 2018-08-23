9318d19cbc520200ecdefcaa23b4d878f5d14644
robertainslie <robert.p.ainslie@gmail.com>
2018-08-22 20:12:28 -0400

    starting oAuth

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Install Test Data Into HubSpot CRM' });
+});
+
+router.get('/success', function(req, res, next) {
+  res.render('success', { title: 'Success!' });
 });
 
 module.exports = router;
