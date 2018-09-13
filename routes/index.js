605272b1a8ee36c6e50a93f9de808a6acc256dd5
robertainslie <robert.p.ainslie@gmail.com>
2018-09-12 21:04:56 -0400

    adding route

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Install Test Data Into HubSpot CRM' });
 });
 
+router.get('/bot', function(req, res, next) {
+	console.log(req.body);
+	res.send(200);
+});
+
 router.get('/auth', function(req, res, next) {
-	console.log(req.query.code);
-	console.log(req.query.code.length);
     if (req.query.code.length > 0) {
     	axios.post('https://api.hubapi.com/oauth/v1/token', qs.stringify({
 		    grant_type:'authorization_code',
@@ -20,10 +23,13 @@ router.get('/auth', function(req, res, next) {
 		    code:req.query.code
 		  }))
 		  .then(function (response) {
-		  	var access_token = response.data.access_token
-		    console.log(response);
+		  	var access_token = response.data.access_token;
 		    res.render('success', { title: 'Success!', access_token:response.data.access_token, auth_message:'You have successfully authenticated. You should begin seeing test data in your portal shortly.'});
+		    console.log('after render after render')
 		  })
+		  .then(
+		  	console.log('in then after render')
+		  	)
 		  .catch(function (error) {
 		    console.log(error);
 		    res.render('success', { title: 'There was an issue.',auth_message:'Something went wrong.' });
@@ -33,5 +39,57 @@ router.get('/auth', function(req, res, next) {
     	res.render('success', { title: 'There was an issue.',auth_message:'Something went wrong.' });
     }
 });
+
+function createCompany (accessToken,hubId) {
+	axios.post(`https://api.hubapi.com/companies/v2/companies`, companyOne, {headers: {'Authorization': `Bearer ${accessToken}`}})
+	.then(function(response){
+		console.log(`Created company ${response.data.companyId}`)
+	})
+	.catch(function (error){
+		console.log(`There was an error. Message: ${error.data}`)
+	})
+}
+
+const companyOne = {
+  "properties": [
+    {
+      "name": "name",
+      "value": "Ministry of Magic"
+    },
+    {
+      "name": "description",
+      "value": "The governmental agency that oversees all matters magical"
+    },
+    {
+      "name": "domain",
+      "value": "ministryofmagic.gov"
+    },
+    {
+      "name": "industry",
+      "value": "government"
+    }
+  ]
+}
+
+const companyTwo = {
+  "properties": [
+    {
+      "name": "name",
+      "value": "Ministry of Magic"
+    },
+    {
+      "name": "description",
+      "value": "The governmental agency that oversees all matters magical"
+    },
+    {
+      "name": "domain",
+      "value": ""
+    },
+    {
+      "name": "industry",
+      "value": "government"
+    }
+  ]
+}
 
 module.exports = router;
